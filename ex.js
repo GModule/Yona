b4442d1a542a91072e24bac16eecbf79d4f85c9c
15.6.4 <117864191+GModule@users.noreply.github.com>
2025-02-27 10:59:52 -0500

    Create ex.js

---
+++ ex.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Novium
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Novium
+// @author       Novium
+// @match        *://*.example.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+})();
